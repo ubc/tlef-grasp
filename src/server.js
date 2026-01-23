a20792130d295f899ab111b5bc2c08f1288d5b70
Kelvin Xu <kelvin.xu@ubc.ca>
2026-01-23 11:07:10 -0800

    Enabled unsafe inline in helmet

--- src/server.js
+++ src/server.js
@@ -39,6 +39,7 @@ app.use(
           "https://cdnjs.cloudflare.com",
           "https://cdn.jsdelivr.net",
         ],
+        scriptSrcAttr: ["'unsafe-inline'"],
         styleSrc: [
           "'self'",
           "'unsafe-inline'",
